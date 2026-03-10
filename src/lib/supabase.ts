5978669e70ca37f23d0ec2dedc158a8f8823470d
はち <skenki01@gmail.com>
2026-03-10 15:30:27 +0900

    supabase key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://kvtteaeodxjkualffemx.supabase.co";
 
-const supabaseAnonKey =
-  "eyJh...ここにSupabaseのANON KEY全部";
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2dHRlYWVvZHhqa3VhbGZmZW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MjY1NDYsImV4cCI6MjA4NTQwMjU0Nn0.D8IYkaYcDVWvkybEiWrP0qUqMpmLclBhzjc4b5ylV9g";
 
 export const supabase = createClient(
   supabaseUrl,
