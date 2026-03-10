ee8b8f9eb8c20e4c30a5e2d102a73ac9348b7ca3
はち <skenki01@gmail.com>
2026-03-10 15:19:49 +0900

    supabase fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ||
-  "https://kvtteaeodxjkualffemx.supabase.co";
+const supabaseUrl = "https://kvtteaeodxjkualffemx.supabase.co";
 
 const supabaseAnonKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  "ここにANON_KEY";
+  "eyJh...ここにSupabaseのANON KEY全部";
 
 export const supabase = createClient(
   supabaseUrl,
