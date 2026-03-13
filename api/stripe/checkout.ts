7109e43997822297ccc13ea4c42503cd53281b37
はち <skenki01@gmail.com>
2026-03-13 18:04:05 +0900

    fix stripe

--- api/stripe/checkout.ts
+++ api/stripe/checkout.ts
@@ -6,47 +6,42 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2023-10-16",
 });
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2023-10-16",
-});
+export default async function handler(req:any,res:any){
 
-export default async function handler(req: any, res: any) {
-
-  if (req.method !== "POST") {
+  if(req.method !== "POST"){
     return res.status(405).end();
   }
 
   const { priceId } = req.body;
 
-  try {
+  try{
 
     const session = await stripe.checkout.sessions.create({
 
-      payment_method_types: ["card"],
+      payment_method_types:["card"],
 
-      line_items: [
+      line_items:[
         {
           price: priceId,
-          quantity: 1,
-        },
+          quantity:1
+        }
       ],
 
-      mode: "payment",
+      mode:"payment",
 
-      success_url: "https://hisohiso.vercel.app/success",
-      cancel_url: "https://hisohiso.vercel.app/about/points",
+      success_url:"https://hisohiso.vercel.app/success",
+      cancel_url:"https://hisohiso.vercel.app/about/points"
 
     });
 
     res.status(200).json({ url: session.url });
 
-  } catch (err: any) {
+  }catch(err:any){
 
     console.error(err);
 
-    res.status(500).json({
-      error: err.message,
-    });
+    res.status(500).json({error:err.message});
 
   }
+
 }
