1a396bb09b7a4ee1cbf9316f06ba7af10fb5edd0
はち <skenki01@gmail.com>
2026-04-05 19:26:33 +0900

    fix gitmodules

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {  Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 
 import InstallGuide from "./components/InstallGuide";
@@ -30,63 +30,48 @@ import Privacy from "./pages/legal/Privacy";
 import Tokushoho from "./pages/legal/Tokushoho";
 import Contact from "./pages/legal/Contact";
 
-/* 🔥 追加 */
-import Pay from "./pages/Pay";
+/* 🔥 ここ修正 */
+import Pay from "./pages/Pay.tsx";
 
 export default function App() {
   return (
     <>
-
       <InstallGuide />
 
       <Routes>
-
-        {/* 初期 */}
         <Route path="/" element={<Navigate to="/register" replace />} />
 
-        {/* 登録 */}
         <Route path="/register" element={<Register />} />
 
-        {/* セレクト */}
         <Route path="/select/boy" element={<BoySelect />} />
         <Route path="/select/girl" element={<GirlSelect />} />
         <Route path="/select/free" element={<FreeSelect />} />
 
-        {/* ラウンジ */}
         <Route path="/lounge" element={<LoungeEnter />} />
         <Route path="/lounge/chat" element={<LoungeChat />} />
 
-        {/* チャット */}
         <Route path="/chat/boy/:id" element={<BoyChat />} />
         <Route path="/chat/girl/:id" element={<GirlChat />} />
         <Route path="/chat/free" element={<FreeChat />} />
 
-        {/* 課金 */}
         <Route path="/purchase/subscription" element={<SubscriptionPurchase />} />
         <Route path="/pay" element={<Pay />} />
 
-        {/* 成功 */}
         <Route path="/purchase/sub-success" element={<SubSuccess />} />
 
-        {/* 説明 */}
         <Route path="/about" element={<About />} />
         <Route path="/about/point" element={<PointAbout />} />
         <Route path="/about/balance" element={<PointBalance />} />
 
-        {/* 法律ページ */}
         <Route path="/terms" element={<Terms />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/tokushoho" element={<Tokushoho />} />
         <Route path="/contact" element={<Contact />} />
 
-        {/* シェア */}
         <Route path="/share" element={<Share />} />
 
-        {/* 不正URL */}
         <Route path="*" element={<Navigate to="/register" replace />} />
-
       </Routes>
-</>
-    
+    </>
   );
 }
